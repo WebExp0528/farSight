19d4c7428d50249c17b81a15ab49dde21b2c3ac1
WebEXP0528 <web.dev0528@gmail.com>
2020-12-21 12:33:06 -0700

    cleaned up script formatting and removed extraneous, redundant line of AWS CLI command.

--- publish.ps1
+++ publish.ps1
@@ -1,12 +1,20 @@
 #Note to newbies: Hashes are comments. 
+
 #set the aws region for AWS Tools
 Set-DefaultAWSRegion -Region us-east-1
+
+#log docker into AWS ECR 
 (Get-ECRLoginCommand).Password | docker login --username AWS --password-stdin 538092687847.dkr.ecr.us-east-1.amazonaws.com
+
+#build the Dockerfile in this folder
 docker build -t far-sight-repo .
+
+#prepare the new docker image for deployment to ECR 
 docker tag far-sight-repo:latest 538092687847.dkr.ecr.us-east-1.amazonaws.com/far-sight-repo:latest
+
+#deliver the docker image to ECR
 docker push 538092687847.dkr.ecr.us-east-1.amazonaws.com/far-sight-repo:latest
+
 #To trigger AWS EB to pull the docker image from ECR we must issue an environment update command.   Here we just set the version-label to the same value to trigger the update. 
 #AWS Tools for PowerShell command to update the elastic beanstalk developer environment.
 Update-EBEnvironment -ApplicationName FarSight -EnvironmentName Farsight-develop -VersionLabel farsight-source-14
-#AWS CLI V2 command to update application version: 
-#aws elasticbeanstalk update-environment --application-name FarSight --environment-name Farsight-develop --version-label farsight-source-14
