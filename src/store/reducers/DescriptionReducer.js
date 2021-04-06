41aff0f630eed5bfc0e8214c534bf6cacfa8ccc1
WebEXP0528 <web.dev0528@gmail.com>
2021-04-06 10:39:14 -0700

    Add comment

--- src/store/reducers/DescriptionReducer.js
+++ src/store/reducers/DescriptionReducer.js
@@ -1,23 +1,28 @@
-import {GET_DETAILS} from '../types'
+import { GET_DETAILS } from "../types";
 
 const initialState = {
-    won:[],
-    loading:true
-}
+  won: [],
+  loading: true,
+};
 
-export default function Descriptionreducer(state = initialState, action){
+/**
+ * Description Reducer
+ *
+ * @param {*} state
+ * @param {*} action
+ * @returns
+ */
+export default function Descriptionreducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_DETAILS:
+      return {
+        //State modified.  must copy.  (... operator)
+        ...state,
+        won: action.payload,
+        loading: false,
+      };
 
-    switch(action.type){
-
-        case GET_DETAILS:
-        return {//State modified.  must copy.  (... operator)
-            ...state,
-            won:action.payload,
-            loading:false
-
-        }
-       
-        default: return state//no modification.  return original state
-    }
-
+    default:
+      return state; //no modification.  return original state
+  }
 }
