e2464f00ddac1a3ddf0711e0dc76b9f6cb5d25bf
WebEXP0528 <web.dev0528@gmail.com>
2021-04-20 16:22:00 -0700

    Update route component

--- src/component/Routes/index.jsx
+++ src/component/Routes/index.jsx
@@ -4,27 +4,32 @@ import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import _ from 'lodash';
 
 import { genRandomCode } from 'helpers';
+import NotFoundPage from 'pages/NotFound';
 
-const RouteWithSubRoutes = ({ path, routes = [], component: Component, match, parentKey, ...rest }) => {
-  routes = routes.map(item => {
+const RouteWithSubRoutes = ({ path, routes = [], parentKey, component: Component, match, ...rest }) => {
+  const newRoutes = _.cloneDeep(routes).map(item => {
     item.key = `${_.snakeCase(parentKey)}_${_.snakeCase(item.key)}`;
     return item;
   });
-  return <Route path={path} render={props => <Component {...props} routes={routes} />} {...rest} />;
+  return <Route path={path} render={props => <Component {...props} routes={newRoutes} />} {...rest} />;
 };
 
-export const RenderRoutes = props => {
+const RenderRoutes = props => {
   const { routes, match } = props;
   return (
     <Switch>
       {routes.map((route, i) => {
         const { key = genRandomCode(), path = '', type = 'route', ...props } = route;
+
         const newPath = match.path === '/' ? `/${path}` : `${match.path}/${path}`;
+
         if (type === 'route') {
           return <RouteWithSubRoutes key={_.snakeCase(key)} parentKey={key} path={newPath} {...props} />;
         } else if (type === 'redirect') {
           return <Redirect key={_.snakeCase(key)} to={newPath} {...props} />;
         }
+
+        return <></>;
       })}
     </Switch>
   );
