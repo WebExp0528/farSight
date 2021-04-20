63794cd269f0bdf51da77878ce88e84fd634f16b
WebEXP0528 <web.dev0528@gmail.com>
2021-04-19 20:31:54 -0700

    Update routes component

--- src/component/Routes/index.jsx
+++ src/component/Routes/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import _ from 'lodash';
+
 import { genRandomCode } from 'helpers';
 
-const RouteWithSubRoutes = ({ path, routes, component: Component, match, ...rest }) => {
+const RouteWithSubRoutes = ({ path, routes = [], component: Component, match, parentKey, ...rest }) => {
+  routes = routes.map(item => {
+    item.key = `${_.snakeCase(parentKey)}_${_.snakeCase(item.key)}`;
+    return item;
+  });
   return <Route path={path} render={props => <Component {...props} routes={routes} />} {...rest} />;
 };
 
@@ -15,9 +21,9 @@ export const RenderRoutes = props => {
         const { key = genRandomCode(), path = '', type = 'route', ...props } = route;
         const newPath = match.path === '/' ? `/${path}` : `${match.path}/${path}`;
         if (type === 'route') {
-          return <RouteWithSubRoutes key={key} path={newPath} {...props} />;
+          return <RouteWithSubRoutes key={_.snakeCase(key)} parentKey={key} path={newPath} {...props} />;
         } else if (type === 'redirect') {
-          return <Redirect key={`${path}-${i}`} to={newPath} {...props} />;
+          return <Redirect key={_.snakeCase(key)} to={newPath} {...props} />;
         }
       })}
     </Switch>
