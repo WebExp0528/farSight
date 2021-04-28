032bf2090f604b667e0d85d5fc70c1d70a74d903
WebEXP0528 <web.dev0528@gmail.com>
2021-04-28 08:58:55 -0700

    Add create action

--- src/@redux/workOrderBids/actions.js
+++ src/@redux/workOrderBids/actions.js
@@ -1,14 +1,15 @@
 import { genActionTypes } from 'helpers';
 
 export const ACTION_NAME = 'work_order_bids';
+const ACTION_TYPES = genActionTypes(ACTION_NAME);
 
 /**
- *
+ * Get bids
  * @param {string | number} id
  * @returns
  */
 export const get = id => ({ axios }) => ({
-  type: genActionTypes(ACTION_NAME).GET,
+  type: ACTION_TYPES.GET,
   meta: {
     toast_success: {
       content: 'Get WorkOrder Bids'
@@ -19,5 +20,20 @@ export const get = id => ({ axios }) => ({
   },
   payload: axios.get(`/api/work_order/${id}/bid`).then(res => {
     return { data: res.data };
+  })
+});
+
+export const create = (id, bidItem) => ({ axios }) => ({
+  type: ACTION_TYPES.CREATE,
+  meta: {
+    toast_success: {
+      content: 'Created WorkOrder Bid Item'
+    },
+    toast_error: {
+      content: 'Could not create WorkOrder Bid Item'
+    }
+  },
+  payload: axios.post(`/api/work_order/${id}/bid`, [{ ...bidItem, bid_item_number: 'new' }]).then(res => {
+    return res.data;
   })
 });
