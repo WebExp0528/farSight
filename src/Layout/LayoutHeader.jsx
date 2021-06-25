602548c34535b181c9fca1682fd6d416b3d5230b
WebEXP0528 <web.dev0528@gmail.com>
2021-06-25 10:58:20 -0700

    no message

--- src/Layout/LayoutHeader.jsx
+++ src/Layout/LayoutHeader.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
-import { Nav, Navbar, Button, Container } from 'react-bootstrap';
+import { Nav, Navbar, Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import BreadCrumb from 'components/BreadCrumb';
-import { axios } from 'helpers';
+// import { axios } from 'helpers';
 
 import cls from './layout.module.scss';
 import { withRouter } from 'react-router';
+import classNames from 'classnames';
 
-const LayoutHeader = props => {
-  const { history } = props;
-  const handleClickDemo = () => {
-    axios
-      .get('/demo', {
-        method: 'GET',
-        headers: {
-          'X-Requested-With': 'XMLHttpRequest'
-        }
-      })
-      .then(_res => {
-        history.push('/');
-      })
-      .catch(_err => {
-        history.push('/');
-      });
-  };
+const LayoutHeader = _props => {
+  // const { history } = props;
+  // const handleClickDemo = () => {
+  //   axios
+  //     .get('/demo', {
+  //       method: 'GET',
+  //       headers: {
+  //         'X-Requested-With': 'XMLHttpRequest'
+  //       }
+  //     })
+  //     .then(_res => {
+  //       history.push('/');
+  //     })
+  //     .catch(_err => {
+  //       history.push('/');
+  //     });
+  // };
   return (
     <Navbar className={cls.navbar} bg="primary" variant="dark" fixed="top">
       <Container fluid>
@@ -32,7 +33,9 @@ const LayoutHeader = props => {
           <BreadCrumb />
         </Nav>
         <Nav>
-          <Navbar.Brand className="d-flex align-items-center">{navigator.platform}</Navbar.Brand>
+          <Navbar.Brand className={classNames('d-flex align-items-center', cls.platformTitle)}>
+            {navigator.platform}
+          </Navbar.Brand>
           <div className={cls.logoWrapper}>
             <FontAwesomeIcon icon={['fas', 'eye']} size="lg" style={{ margin: 0, padding: 0, color: 'white' }} />
             <FontAwesomeIcon icon={['fas', 'chess-rook']} size="2x" style={{ margin: 0, padding: 0, color: 'white' }} />
