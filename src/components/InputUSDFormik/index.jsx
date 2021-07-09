8f57a6d171ce81d609280b5a620f10a1e3080d29
WebEXP0528 <web.dev0528@gmail.com>
2021-07-09 14:59:17 -0700

    Added validation in price element

--- src/components/InputUSDFormik/index.jsx
+++ src/components/InputUSDFormik/index.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import { InputGroup, FormControl } from 'react-bootstrap';
+import { useField } from 'formik';
+import { InputGroup, FormControl, Form } from 'react-bootstrap';
 
-import withFormikField from 'components/WithFormikField';
-
-export const InputUSDFormik = withFormikField()(props => {
+export const InputUSDFormik = props => {
+  let { label, labelProps, name, helperProps, helperType = 'invalid', ...rest } = props;
+  const [field, meta] = useField(name);
   return (
-    <InputGroup>
-      <InputGroup.Text>$</InputGroup.Text>
-      <FormControl {...props} />
-      <InputGroup.Text>.00</InputGroup.Text>
-    </InputGroup>
+    <Form.Group className="my-2">
+      {label && <Form.Label {...labelProps}>{label}</Form.Label>}
+      <InputGroup hasValidation>
+        <InputGroup.Text>$</InputGroup.Text>
+        <FormControl
+          name={name}
+          isInvalid={meta.touched && meta.error}
+          value={field.value}
+          onChange={field.onChange}
+          onBlur={field.onBlur}
+          {...rest}
+        />
+        <InputGroup.Text>.00</InputGroup.Text>
+        {meta.touched && meta.error && (
+          <Form.Control.Feedback type={helperType} {...helperProps}>
+            {meta.error}
+          </Form.Control.Feedback>
+        )}
+      </InputGroup>
+    </Form.Group>
   );
-});
+};
