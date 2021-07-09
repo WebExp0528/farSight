f690d6a0c46a53f2fbfbb3326fa447be5fcce4b7
WebEXP0528 <web.dev0528@gmail.com>
2021-07-09 13:48:29 -0700

    Remove test code

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const NS_FS_API = process.env.NS_FS_API;
 
 app.use(
   cors({
-    origin: process.env.NODE_ENV === 'development' ? ['http://localhost:3000', 'http://192.168.1.232:3000'] : true,
+    origin: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : true,
     credentials: true,
     exposedHeaders: 'X-VENDOR-NAME'
   })
